049571c8982ab1c7a79b4315cbd276668bc669eb
Simon <simjenner3@gmail.com>
2018-01-14 16:08:58 +0000

    Update

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 echo ! Has README.rst been updated? !
 pause
 echo Building /dist
+rm -rf build dist
 python setup.py sdist bdist_wheel
 echo -----------------------------
 echo Press ENTER to upload to PyPi
