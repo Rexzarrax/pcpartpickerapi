e00c96bd869db1ce08140655e3a04e57d07c5f03
thatguywiththatname <simjenner3@gmail.com>
2019-03-25 17:28:20 +0000

    Bump PyPi version

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -11,16 +12,16 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 
 setup(
     name="PCPartPicker_API",
-    version="0.0.9",
+    version="0.1.0",
     description="An unofficial API to access & scrape data from pcpartpicker.com",
     long_description=long_description,
     url="https://github.com/thatguywiththatname/PcPartPicker-API",
     classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
     ],
     keywords="PCPartPicker API scraping",
     packages=find_packages(exclude=["contrib", "docs", "tests", "examples"]),
-    install_requires=["beautifulsoup4", "requests"]
+    install_requires=["beautifulsoup4", "requests"],
 )
