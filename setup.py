c7512f72e4135c3355f08bf5f42af1514329f778
thatguywiththatname <simjenner3@gmail.com>
2019-05-07 15:13:45 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
 
 setup(
     name="pcpartpickerapi",
-    version="0.1.5",
+    version="0.1.6",
     description="An unofficial Py3 API to scrape and retrieve information from PCPartPicker",
     long_description=long_description,
     long_description_content_type="text/markdown",
