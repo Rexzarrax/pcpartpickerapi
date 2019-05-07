8698ef6cad6ab57039f8315e521e1d31e221ad5d
thatguywiththatname <simjenner3@gmail.com>
2019-05-07 15:25:51 +0100

    Fix CRLF PyPi issue

--- setup.py
+++ setup.py
@@ -8,16 +8,17 @@ here = path.abspath(path.dirname(__file__))
 
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
+    # If on win, get rid of CR from CRLF
+    long_description = long_description.replace("\r", "")
 
 with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
     requirements = f.read()
-    # If on win, get rid of CR
     requirements = requirements.replace("\r", "")
     requirements = requirements.split("\n")
 
 setup(
     name="pcpartpickerapi",
-    version="0.1.6",
+    version="0.1.7",
     description="An unofficial Py3 API to scrape and retrieve information from PCPartPicker",
     long_description=long_description,
     long_description_content_type="text/markdown",
