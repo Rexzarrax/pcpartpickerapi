4194de3da2ab07ce088aa0d1cc133c0cf6da5c92
thatguywiththatname <simjenner3@gmail.com>
2019-05-08 17:25:08 +0100

    Working using pandas, update README, bump version, etc.

--- pcpartpickerapi/part_lists.py
+++ pcpartpickerapi/part_lists.py
@@ -3,38 +3,46 @@ Scrape and retrieve information from pcpartpicker part lists
 For example: https://pcpartpicker.com/products/cpu-cooler/
 """
 
-from .__parts_data import part_lookup
+from .__parts_data import part_type_column_names
 from json import loads as jsonloads
-from bs4 import BeautifulSoup, NavigableString
+import pandas as pd
 import requests
 
+supported_part_types = list(part_type_column_names.keys())
+supported_regions = [
+    "au",
+    "be",
+    "ca",
+    "de",
+    "es",
+    "fr",
+    "in",
+    "ie",
+    "it",
+    "nz",
+    "uk",
+    "us",
+]
 
-def __construct_url(part_type, page_num, region, part_filter):
+
+def __construct_url(part_type, page, region, part_filter):
     """
     Returns the specific pcpartpicker URL for the given parameters
     """
     region = region.lower()
-    if region not in [
-        "au",
-        "be",
-        "ca",
-        "de",
-        "es",
-        "fr",
-        "in",
-        "ie",
-        "it",
-        "nz",
-        "uk",
-        "us",
-    ]:
+
+    # TODO: Is this the best place to do these checks?
+    if region not in supported_regions:
         raise ValueError(f'"{region}" is an invalid / unrecognized region')
+
+    if part_type not in supported_part_types:
+        raise ValueError(f'"{part_type}" is an invalid / unrecognized part_type')
 
     pcpp_url = (
         "https://" + ("" if region == "us" else (region + ".")) + "pcpartpicker.com"
     )
 
-    pcpp_url += f"/products/{part_type}/fetch/?page={page_num}"
+    pcpp_url += f"/products/{part_type}/fetch/?page={page}"
 
     if part_filter.strip() != "":
         pcpp_url += f"&{part_filter}"
@@ -42,26 +50,13 @@ def __construct_url(part_type, page_num, region, part_filter):
     return pcpp_url
 
 
-def __get_json(part_type, page_num, region, part_filter):
+def __get_json(part_type, page, region, part_filter):
     """
     Returns the JSON from pcpartpicker for the given parameters
     """
-    if part_type not in part_lookup:
-        raise ValueError(f'"{part_type}" is an invalid / unrecognized part_type')
-
-    pcpp_url = __construct_url(part_type, page_num, region, part_filter)
+    pcpp_url = __construct_url(part_type, page, region, part_filter)
     r = requests.get(pcpp_url)
-    parsed = jsonloads(r.content.decode("utf-8"))
-
-    return parsed
-
-
-def __get_list_html(part_type, page_num, region, part_filter):
-    """
-    Returns a BeautifulSoup object for the page for the given parameters
-    """
-    data = __get_json(part_type, page_num, region, part_filter)
-    return BeautifulSoup(data["result"]["html"], "html.parser")
+    return jsonloads(r.content.decode("utf-8"))
 
 
 def list_info(part_type, region="us", part_filter=""):
@@ -76,67 +71,58 @@ def list_info(part_type, region="us", part_filter=""):
     return {"total_part_count": total_part_count, "page_count": page_count}
 
 
-def get_list(part_type, page_num=0, region="us", part_filter=""):
+def get_list(part_type, page=0, region="us", part_filter=""):
     """
     Returns a list of dictionaries containing information about each part
-    If page_num is left to the default (0), it gets all pages
+    If page is left to the default (0), it gets all pages
     The pages start at 1
     """
-    if page_num == 0:
+    if page == 0:
         part_page_count = list_info(part_type, region, part_filter)["page_count"]
-        start_page_num, end_page_num = 1, part_page_count
+        start_page, end_page = 1, part_page_count
     else:
-        start_page_num, end_page_num = page_num, page_num
+        start_page, end_page = page, page
 
+    column_names = part_type_column_names[part_type]
     part_dict_list = []
-    for page_num in range(start_page_num, end_page_num + 1):
 
-        # For each page requested, get the bs4 soup for that page
-        soup = __get_list_html(part_type, page_num, region, part_filter)
+    for page in range(start_page, end_page + 1):
 
-        # For each row in the table, pull the known data
-        for row in soup.find_all("tr"):
+        data = __get_json(part_type, page, region, part_filter)
 
-            part_details = {}
-            for count, value in enumerate(row):
+        html_table = f"<table>{data['result']['html']}</table>"
 
-                # Each part has a HTML table that contains the information about it.
-                # Some rows don't matter, and others have a value. The ones with values
-                # always have a value, and for each partType, contain specific data.
-                # For example, the 3rd row for any part always contains the parts name.
-                # The part_lookup dict contains {row number : data name} pairs that
-                # allow you to determine what row contains what data.
+        table_df = pd.read_html(html_table)[0]
+        # Replace "NaN" values with None
+        table_df = table_df.replace({pd.np.nan: None})
 
-                if isinstance(value, NavigableString):
-                    continue  # Skip blank strings
+        table_row_count = table_df.shape[0]
+        table_dict = table_df.to_dict()
 
-                text = value.get_text().strip()
+        print(table_df)
 
-                if count in part_lookup[part_type]:
-                    part_details[part_lookup[part_type][count]] = text
+        for row_num in range(table_row_count):
 
-                elif count == 1:
-                    part_details["id"] = value.input["id"]
-                elif count == 3:
-                    part_details["name"] = text
-                elif count == len(row) - 3:
-                    part_details["price"] = text
-                elif count == len(row) - 5:
-                    # The rating counts are surrounded by ()
-                    part_details["ratings_count"] = text.replace("(", "").replace(
-                        ")", ""
-                    )
+            part_dict = {}
 
-            part_dict_list.append(part_details)
+            for col_num in range(len(column_names)):
+
+                col_name = column_names[col_num]
+
+                # +1 because the 0th column is the row number
+                col_value = table_dict[col_num + 1][row_num]
+
+                # TODO: Is this the best way to do this?
+                if col_name == "ratings_count":
+                    # Remove "()" from the ratings count
+                    col_value = col_value.replace("(", "")
+                    col_value = col_value.replace(")", "")
+
+                part_dict[col_name] = col_value
+
+            part_dict_list.append(part_dict)
 
     return part_dict_list
-
-
-def supported_part_types():
-    """
-    Returns a list of supported part_types
-    """
-    return list(part_lookup.keys())
 
 
 def supported_keys(part_type):
@@ -144,9 +130,4 @@ def supported_keys(part_type):
     Returns a list of dictionary keys that the dictionaries from get_list will have
     for that part_type
     """
-    keys = list(part_lookup[part_type].values())
-    keys.append("name")
-    keys.append("id")
-    keys.append("price")
-    keys.append("ratings_count")
-    return keys
+    return part_type_column_names[part_type]
--- setup.py
+++ setup.py
@@ -6,19 +6,23 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
-    long_description = f.read()
-    # If on win, get rid of CR from CRLF
-    long_description = long_description.replace("\r", "")
 
-with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
-    requirements = f.read()
-    requirements = requirements.replace("\r", "")
-    requirements = requirements.split("\n")
+def read_local_file(f_name):
+    with open(path.join(here, f_name), encoding="utf-8") as f:
+        contents = f.read()
+    # If on win, get rid of CR from CRLF
+    contents = contents.replace("\r", "")
+    return contents
+
+
+long_description = read_local_file("README.md")
+
+requirements = read_local_file("requirements.txt")
+requirements = requirements.split("\n")
 
 setup(
     name="pcpartpickerapi",
-    version="0.1.7",
+    version="0.1.8",
     description="An unofficial Py3 API to scrape and retrieve information from PCPartPicker",
     long_description=long_description,
     long_description_content_type="text/markdown",
